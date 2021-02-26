3aecc4c4ac71a9fe69c9a126e2d3c8380107e034
George <ubong_george@ymail.com>
2021-02-26 20:17:39 +0100

    Add class and attribute accessors
    Also created an instance variable to hold a collection of upper body exercises

---
+++ lib/wellness.rb
@@ -0,0 +1,13 @@
+class WellnessTips
+  attr_reader :upper_body, :lower_body, :tips_for_toning, :tips_for_weight_loss
+  @upper_body = %(
+    Try to incorporate these in your next work-out session
+    - Overhead Presses
+    - Pull-ups
+    - Seated Cable Rows
+    - Dumbbell Bench Presses
+    - Dumbbell Bicep Curls 
+    - Side Planks
+    - Pushups
+  )
+end
